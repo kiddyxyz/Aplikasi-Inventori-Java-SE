a058a730082979d695ff42fff5ba267b72cc0b36
Kiddy <kiddydhana@gmail.com>
2019-04-09 14:55:47 +0700

    Add Create Feature.

---
+++ src/ujicobainventori/Controller/MainController.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ujicobainventori.Controller;
+
+import java.awt.Color;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTable;
+import net.proteanit.sql.DbUtils;
+import ujicobainventori.Connection.Koneksi;
+
+/**
+ *
+ * @author Kiddy
+ */
+public class MainController {
+   
+    public void get(JTable table, String sql) {
+        try{
+            Koneksi koneksi = new Koneksi();
+            Connection con = koneksi.open();
+            
+            Statement state = con.createStatement();
+            ResultSet rs = state.executeQuery(sql);
+            
+            table.setModel(DbUtils.resultSetToTableModel(rs));
+            
+            con.close();
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public boolean preparedStatement(Map map, String sql){
+        try{
+            Koneksi koneksi = new Koneksi();
+            Connection con = koneksi.open();
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            map.forEach((k,v) -> {
+                try {
+                    ps.setString((int) k, v.toString());
+                } catch (SQLException ex) {
+                    Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+            
+            int rows = ps.executeUpdate();
+            System.out.println("prepared Statement Executed!");
+            return rows != 0;
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+            return false;
+        }
+    }
+    
+}
